c3696dd1eb174f2acb2565623fe1a3998c6e89f0
Sebastian Fredes <s.fredes@duocuc.cl>
2026-03-12 15:00:44 -0300

    Cambios al controller

---
+++ src/main/java/com/proyectoreserva/reservacancha/controller/CanchaController.java
@@ -0,0 +1,31 @@
+package com.proyectoreserva.reservacancha.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.proyectoreserva.reservacancha.model.Cancha;
+
+@RestController
+@RequestMapping("/canchas")
+public class CanchaController {
+
+    private List<Cancha> canchas = new ArrayList<>();
+
+    @GetMapping
+    public List<Cancha> listarCanchas() {
+        return canchas;
+    }
+
+    @PostMapping
+    public Cancha crearCancha(@RequestBody Cancha cancha) {
+        
+
+}
+
+}
--- src/main/java/com/proyectoreserva/reservacancha/controller/controller.java
+++
@@ -1,5 +0,0 @@
-package com.proyectoreserva.reservacancha.controller;
-
-public class controller {
-    
-}
