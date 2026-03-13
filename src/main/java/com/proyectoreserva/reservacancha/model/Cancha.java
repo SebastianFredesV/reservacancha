f6b8a1a42898fad12596410834a8eda996739431
Sebastian Fredes <s.fredes@duocuc.cl>
2026-03-13 12:39:26 -0300

    Añadidos application properties para la conexión con base de datos y dependencias jakarta

--- src/main/java/com/proyectoreserva/reservacancha/model/Cancha.java
+++ src/main/java/com/proyectoreserva/reservacancha/model/Cancha.java
@@ -3,13 +3,20 @@ package com.proyectoreserva.reservacancha.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
 
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 
 public class Cancha {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
+    
     private String nombre;
     private String tipo;
     private int precioPorHora;
